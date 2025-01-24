e9e35cd9d05890e49bb345d76576c3adf8bb4bd8
josephmv43 <josephmv43@gamil.com>
2025-01-24 22:21:26 +0530

    feat:Add creatMenu function in menuController

--- backend/src/controller/menuController.ts
+++ backend/src/controller/menuController.ts
@@ -1,11 +1,16 @@
 
 
 import { Request, Response } from "express";
+import Menu from "../model/menu";
 
 
 export const createMenu = async (req: Request, res: Response) => {
   try {
-  console.log(req.body)
+    const menu = new Menu(req.body);
+    console.log(menu)
+    const savedMenu = await menu.save();
+    const savedMenuId = savedMenu._id; 
+    res.status(201).json(savedMenuId);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
